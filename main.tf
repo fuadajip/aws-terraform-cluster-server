08136ee3a49a09272962b762d88a22c8a0e17da0
fuadajip <fuadajipratomo@gmail.com>
2020-01-06 17:36:25 +0700

    [FINALIZE] Finalize ec2 cluster with autoscale

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=0.12"
+  required_version = ">= 0.12"
 }
 
 provider "aws" {
@@ -9,28 +9,14 @@ provider "aws" {
 
 data "aws_availability_zones" "az-available-all" {}
 
-# create ec2 instance
-resource "aws_launch_configuration" "iot-ec2-lg" {
-  ami             = "ami-08569b978cc4dfa10"
-  instance_type   = "t2.micro"
-  security_groups = []
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-
 # create auto scalling group configuration for the instane
 resource "aws_autoscaling_group" "iot-ec2-asg" {
-  launch_configuration = aws_launch_configuration.iot-ec2-lg
+  launch_configuration = aws_launch_configuration.iot-ec2-test-lg.id
   availability_zones   = data.aws_availability_zones.az-available-all.names
+
+  # mapping each registered instance in asg to elb
+  load_balancers    = [aws_elb.iot-elb-test.name]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 3
@@ -41,18 +27,91 @@ resource "aws_autoscaling_group" "iot-ec2-asg" {
   }
 }
 
+
+# create ec2 instance
+resource "aws_launch_configuration" "iot-ec2-test-lg" {
+  image_id        = "ami-08569b978cc4dfa10"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.iot-ec2-test-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+
+# create security group for the web server
+resource "aws_security_group" "iot-ec2-test-sg" {
+  name = "iot-ec2-test-sg"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
+
+# ---------------------
+
+
 # create http alb
 resource "aws_elb" "iot-elb-test" {
   name               = "iot-elb-test"
   availability_zones = data.aws_availability_zones.az-available-all.names
+  security_groups    = [aws_security_group.iot-elb-test-sg.id]
+
+  # create healthcheck for web server instance
+  health_check {
+    target              = "http:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
 
   # routes listener for incoming http request
   listener {
     lb_port           = 80
     lb_protocol       = "http"
-    instance_port     = var.server_ports
+    instance_port     = var.server_port
     instance_protocol = "http"
   }
 }
+
+# create security group for alb
+resource "aws_security_group" "iot-elb-test-sg" {
+  name = "iot-elb-test-sg"
+
+  // allow outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // allow inbound
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "iot-elb-test-name" {
+  value       = "aws_elb.iot-elb-test.dns_name"
+  description = "The domain name of aws elb"
+}
